9a2edc897b594a2bbf7ca1e52780816b26364c84
dimaas1 <dimonmad@gmail.com>
2026-03-06 20:40:43 +0200

    add file

---
+++ 4.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+shape("turtle")
+pensize(10)
+
+forward(100)
+left(90)
+forward(100)
+
+penup()
+color("red", "yellow")
+pendown()
+
+left(90)
+forward(100)
+left(180)
+forward(150)
+
+done()
---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle
+pen = turtle.Turtle()
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+turtle.done()
